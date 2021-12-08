aa73a9e592ad10ee709e5e922019bcc4e32ea86b
namtiennguyen97 <toilanam97@gmail.com>
2021-12-08 08:25:36 +0700

    make it better

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+// const host = 'https://fleetingwords.herokuapp.com/';
+let port = 80
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
